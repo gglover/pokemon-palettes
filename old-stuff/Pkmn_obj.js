63cd3c751c95aa803073afcfdc88c3142af6da1c
Gus Glover <gus.glov3r@gmail.com>
2020-11-11 21:12:57 -0800

    update for github pages hosting

--- old-stuff/Pkmn_obj.js
+++
@@ -1,9 +0,0 @@
-function Pokemon(name, number) {
-	this.number = number;
-	this.name = name;
-	this.colors = {};
-}
-
-Pokemon.prototype.addColor = function(color, amount) {
-	colors[color] = amount;
-}
